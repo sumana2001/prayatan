4cfc14095679e1988acc0258f24565eef642f97f
Sumana Basu <sumana.basu2001@gmail.com>
2021-08-18 17:22:11 +0530

    minor

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,13 +7,13 @@ function Hero(){
         return (
             <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true}  showStatus={false}>
                 <div>
-                    <img className="hero-img" src="https://images.unsplash.com/flagged/photo-1569118311341-145748f3f338?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmdvfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80" />
+                    <img className="hero-img" src="https://images.unsplash.com/flagged/photo-1569118311341-145748f3f338?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmdvfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80" alt="img"/>
                 </div>
                 <div>
-                    <img className="hero-img" src="https://images.unsplash.com/photo-1594708767771-a7502209ff51?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5nb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+                    <img className="hero-img" src="https://images.unsplash.com/photo-1594708767771-a7502209ff51?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5nb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="img"/>
                 </div>
                 <div>
-                    <img className="hero-img" src="https://images.unsplash.com/photo-1486280380840-0531135d75af?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTE2NTM0fHxlbnwwfHx8&ixlib=rb-1.2.1&w=1000&q=80" />
+                    <img className="hero-img" src="https://images.unsplash.com/photo-1486280380840-0531135d75af?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTE2NTM0fHxlbnwwfHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="img"/>
                 </div>
             </Carousel>
         );
