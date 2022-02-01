21003fd7664f0215513628b49ed13533045a1279
sumana2001 <sumana.basu2001@gmail.com>
2022-02-01 12:38:27 +0530

    Navbar

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import styled from "styled-components";
 
 const LogoWrapper = styled.div`
-  display: flex;
+  display: float;
   align-items: center;
 `;
 
 const LogoText = styled.h1`
   color: #fff;
+  float:right;
+  @media screen and (max-width: 768px) {
+    font-size:1.5rem;
+  }
 `;
+
+const Image = styled.img`
+max-height:40px;
+float:left;
+@media screen and (max-width: 768px) {
+  
+}
+`
 
 export function Logo(props) {
   return (
     <LogoWrapper>
       <a href="/" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-        <LogoText>
-          Prayatan<span style={{ color: "#fdd831" }}>.</span>
+      <LogoText>
+      <Image src="https://res.cloudinary.com/dqlh4eyt8/image/upload/v1643698497/logo-modified_yuaqs7.png" alt="Prayatan Logo" />
+        
+          &nbsp;Prayatan-Ek Awaaz
         </LogoText>
       </a>
     </LogoWrapper>
